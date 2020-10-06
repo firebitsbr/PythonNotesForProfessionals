fb25c0949d64d47752cfa0524c1707f73eba5cd5
Mauro Risonho de Paula Assumpção <mauro.risonho@gmail.com>
2020-10-05 21:24:42 -0300

    comparação poroperadores logicos

---
+++ page_0036_0001.py
@@ -0,0 +1,10 @@
+x = 0
+y = 1
+
+x or y      # if x is False then y otherwise x  #se x é falso então y caso contrário x
+x and y     # if x is False then y otherwise y  #se x for False, então y, caso contrário, y
+not y       # if x is True then False otherwise True  #se x for verdadeiro, então falso, caso contrário, verdadeiro 
+
+print(x or y)   #se x é falso então y caso contrário x
+print(x and y)  #se x for False, então y, caso contrário, y
+print(not y)    #se x for verdadeiro, então falso, caso contrário, verdadeiro 
